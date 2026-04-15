58f6397ff77b736d7373d64e5e18b391ef324e98
Romansonu <sonusunar2580@gmail.com>
2026-04-14 21:22:25 -0700

    Create Home component with LandingPage and ToolSection

---
+++ app/page.js
@@ -0,0 +1,12 @@
+'use client';
+import LandingPage from '../components/LandingPage';
+import ToolSection from '../components/ToolSection';
+
+export default function Home() {
+  return (
+    <main>
+      <LandingPage />
+      <ToolSection />
+    </main>
+  );
+}
